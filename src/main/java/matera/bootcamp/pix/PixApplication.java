f7d7cfdb0faca2705e4ec405a9ba8f77091a1cc7
Leonardo Araujo Silva <leonardo.araujo@matera.com>
2021-09-27 20:02:49 -0300

    Meu primeiro commit

---
+++ src/main/java/matera/bootcamp/pix/PixApplication.java
@@ -0,0 +1,13 @@
+package matera.bootcamp.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matera/bootcamp/pix/PixApplicationTests.java
@@ -0,0 +1,13 @@
+package matera.bootcamp.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
