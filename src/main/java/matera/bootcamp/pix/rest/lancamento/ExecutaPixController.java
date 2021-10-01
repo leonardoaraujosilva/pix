62cb3d7c11ec5e233934621a5f775cc2a3b91ea2
Leonardo Silva <leonardoaraujosilva3@gmail.com>
2021-10-01 20:53:22 -0300

    Adicionando comentários do passo a passo de como executar uma operação de PIX

--- src/main/java/matera/bootcamp/pix/rest/lancamento/ExecutaPixController.java
+++ src/main/java/matera/bootcamp/pix/rest/lancamento/ExecutaPixController.java
@@ -25,5 +25,63 @@ public class ExecutaPixController {
                 .build();
     }
 
+    /**
+     *
+     * Pra executar um PIX precisamos (exemplos abaixo das linhas do passo a passo):
+     *  1. Criar um usuário com conta corrente
+     *     POST - http://localhost:8080/api/v1/usuario
+     *     {
+     *         "nome": "Leonardo",
+     *         "sobrenome": "Silva",
+     *         "contaCorrente": {
+     *             "agencia": 1,
+     *             "conta": 100,
+     *             "saldo": 1000.00
+     *         }
+     *     }
+     *
+     *  2. Criar um usuário com conta corrente e saldo suficiente pra mandar o PIX
+     *     POST - http://localhost:8080/api/v1/usuario
+     *     {
+     *         "nome": "Ederlon",
+     *         "sobrenome": "Barbosa",
+     *         "contaCorrente": {
+     *             "agencia": 1,
+     *             "conta": 300,
+     *             "saldo": 15000.00
+     *         }
+     *     }
+     *
+     *  3. Cadastrar uma chave para o usuário recebedor
+     *      POST - http://localhost:8080/api/v1/chave
+     *      {
+     *          "tipoChave": "EMAIL",
+     *          "valor": "leonardo.araujo@matera.com",
+     *          "contaCorrente": {
+     *              "id": 1
+     *          }
+     *      }
+     *
+     *  4. Executar PIX informando a chave do recebedor e agencia e conta do pagador
+     *     POST - http://localhost:8080/api/v1/pix
+     *     {
+     *          "chave": "leonardo.araujo@matera.com",
+     *          "valor": "15000.00",
+     *          "pagador": {
+     *              "agencia": 1,
+     *              "conta": 1000
+     *          }
+     *     }
+     *
+     *  5. Consultar saldo das contas dos usuários
+     *      5.1 Para o recebedor
+     *         GET - http://localhost:8080/api/v1/usuario/1/conta
+     *      5.2 Para o pagador
+     *         GET - http://localhost:8080/api/v1/usuario/2/conta
+     *      5.3 Pode também consultar no banco em http://localhost:8080/h2-console
+     *         Executar o seguinte select vai mostrar dados do usuário e conta com saldo
+     *         select * from USUARIO u inner join CONTA_CORRENTE cc ON cc.ID = u.CONTA_CORRENTE_ID
+     *
+     */
 
 }
